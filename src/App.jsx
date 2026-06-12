36751be6c450b23ebe2e925c94fdef9682b14928
sejansorker <sejansorker@gmail.com>
2026-06-13 03:00:23 +0600

    tailwind css add

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ function App() {
 
   return (
     <>
-    <div className="">
-      hello
+    <div className="h-screen flex justify-center items-center">
+      <h1 className="text-[50px] rotate-45 uppercase">Restart</h1>
+      <h2 className="text-[50px] rotate-135 uppercase">restart</h2>
     </div>
     </>
   )
 }
-
 export default App
