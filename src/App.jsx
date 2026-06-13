1351ca156f458bc17bcc5b2e6b03f8d66e85d0b0
sejansorker <sejansorker@gmail.com>
2026-06-13 19:27:20 +0600

    container add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import Container from "./components/Container"
+
 function App() {
 
   return (
     <>
+    <Container>
     <div className="h-screen flex justify-center items-center">
       <h1 className="text-[50px] rotate-45 uppercase">Restart</h1>
       <h2 className="text-[50px] rotate-135 uppercase">restart</h2>
     </div>
+    </Container>
     </>
   )
 }
