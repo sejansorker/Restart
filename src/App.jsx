25aadd67f0e4e2b91b5113b859ea7dd86ecc6432
sejansorker <sejansorker@gmail.com>
2026-06-14 23:57:46 +0600

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
 import Container from "./components/Container"
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
-    <Container>
-    <div className="h-screen flex justify-center items-center">
-      <h1 className="text-[50px] rotate-45 uppercase">Restart</h1>
-      <h2 className="text-[50px] rotate-135 uppercase">restart</h2>
-    </div>
-    </Container>
+   <Header/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Container from './Container'
+
+const Header = () => {
+  return (
+    <div>
+        <Container>
+            header
+        </Container>
+    </div>
+  )
+}
+
+export default Header
