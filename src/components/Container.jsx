bbf95809222d9578f867d20af53f916afb9dcb71
sejansorker <sejansorker@gmail.com>
2026-06-16 02:33:10 +0600

    header html

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({children}) => {
   return (
-    <div className='max-w-320 mx-auto'>{children}</div>
+    <div className='max-w-325 mx-auto'>{children}</div>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import Container from './Container'
-
+import Logo from "../assets/logo.png"
 const Header = () => {
   return (
     <div>
-        <Container>
-            header
-        </Container>
+      <Container>
+        <div className="flex">
+          <div className="w-1/6">
+            <img src={Logo} alt="" />
+          </div>
+          <div className="w-3/6">
+            <div className="">
+              <ul>
+                <li>Home</li>
+                <li>Booking</li>
+                <li>Offers</li>
+                <li>Blog</li>
+                <li>Contact</li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-2/6">
+          <div className="">
+           <p>-400173 464 799</p>
+           <button>Book Now</button>
+          </div>
+          </div>
+        </div>
+      </Container>
     </div>
   )
 }
