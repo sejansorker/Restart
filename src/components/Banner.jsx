22adc0266d378d06b51a1f90a86966aef6ac68bb
sejansorker <sejansorker@gmail.com>
2026-06-17 20:36:22 +0600

    luxury

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ const Banner = () => {
           <div className="w-2/3 text-center">
             <h5 className='text-[20px] text-white font-main font-medium  '>PRINIOM HOTEL & RESORT</h5>
             <h1 className='text-[64px] text-white font-main font-bold leading-tight py-5'>DISCOVER LUXURY <span className='text-[#C4995D] '>BEYOND COMPARE</span></h1>
-            <h4 className='text-[20px] text-white font-main font-medium '>Unwind in elegance and comfort with world-class
+            <h4 className='text-[20px] text-white font-main font-medium lg:px-50 '>Unwind in elegance and comfort with world-class
               service and unforgettable experiences.</h4>
           </div>
         </div>
