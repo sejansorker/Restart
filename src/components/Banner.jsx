9b30922893d165005d03bed6141f2bf4c7c81bc9
sejansorker <sejansorker@gmail.com>
2026-06-18 01:56:42 +0600

    banner complete

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -7,9 +7,9 @@ const Banner = () => {
       <Container>
         <div className="flex justify-center">
           <div className="w-2/3 text-center">
-            <h5 className='text-[20px] text-white font-main font-medium  '>PRINIOM HOTEL & RESORT</h5>
-            <h1 className='text-[64px] text-white font-main font-bold leading-tight py-5'>DISCOVER LUXURY <span className='text-[#C4995D] '>BEYOND COMPARE</span></h1>
-            <h4 className='text-[20px] text-white font-main font-medium lg:px-50 '>Unwind in elegance and comfort with world-class
+            <h5 className='lg:text-[20px] text-[14px] text-white font-main lg:font-medium  font-normal'>PRINIOM HOTEL & RESORT</h5>
+            <h1 className='lg:text-[64px] text-[34px] text-white font-main font-bold leading-tight lg:py-5 py-2'>DISCOVER LUXURY <span className='text-[#C4995D] '>BEYOND COMPARE</span></h1>
+            <h4 className='lg:text-[20px] text-[14px] text-white font-main lg:font-medium  font-normal lg:px-50 '>Unwind in elegance and comfort with world-class
               service and unforgettable experiences.</h4>
           </div>
         </div>
