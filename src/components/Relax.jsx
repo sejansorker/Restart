325de3ca0cc2af10369578bee58ff87c04fdd89a
sejansorker <sejansorker@gmail.com>
2026-06-20 02:47:32 +0600

    relax fixed

--- src/components/Relax.jsx
+++ src/components/Relax.jsx
@@ -10,43 +10,39 @@ const LuxuryComfort = () => {
             <Container>
                 <div className="flex flex-col lg:flex-row gap-4">
                     <div className="w-full lg:w-1/3 text-white flex flex-col justify-center">
-                        <p className="uppercase text-xs tracking-[3px] text-[#C4995D]">
+                        <p className="uppercase font-main font-medium text-[18px] tracking-[3px] text-[#C4995D]">
                             Luxury & Comfort
                         </p>
-                        <h2 className="text-5xl leading-tight font-serif mt-3">
+                        <h2 className="text-5xl leading-tight font-main font-semibold mt-3">
                             Relax. Refresh. Rejuvenate.
                         </h2>
-                        <p className="mt-5 text-gray-300">
+                        <p className="mt-5 text-gray-300 font-main font-normal text-[18px] ">
                             Enjoy premium amenities designed for your ultimate relaxation
                             and comfort.
                         </p>
-                        <button className="mt-8 border border-white w-fit px-6 py-3 rounded-md">
+                        <button className="mt-8 border border-white w-fit px-6 py-3 rounded-md text-[17px]">
                             Learn More
                         </button>
                     </div>
                     <div className="w-full lg:w-1/3">
-                        <img
-                            src={Hotel}
-                            alt=""
-                            className="w-full h-full min-h-[350px] object-cover rounded-md"
-                        />
+                        <img  src={Hotel} className="w-full h-full min-h-[350px] object-cover rounded-md" />
                     </div>
                     <div className="w-full lg:w-1/3 grid grid-cols-2 gap-1">
                         <div className="bg-white rounded-md flex flex-col justify-center items-center py-10">
                             <FaSwimmingPool className="text-[#C4995D] text-2xl" />
-                            <p className="mt-3">Infinity Pool</p>
+                            <p className="mt-3 font-main font-normal text-[18px]">Infinity Pool</p>
                         </div>
                         <div className="bg-white rounded-md flex flex-col justify-center items-center py-10">
                             <FaUtensils className="text-[#C4995D] text-2xl" />
-                            <p className="mt-3">Fine Dining</p>
+                            <p className="mt-3 font-main font-normal text-[18px]">Fine Dining</p>
                         </div>
                         <div className="bg-white rounded-md flex flex-col justify-center items-center py-10">
                             <FaSpa className="text-[#C4995D] text-2xl" />
-                            <p className="mt-3">Spa Center</p>
+                            <p className="mt-3 font-main font-normal text-[18px]">Spa Center</p>
                         </div>
                         <div className="bg-white rounded-md flex flex-col justify-center items-center py-10">
                             <FaDumbbell className="text-[#C4995D] text-2xl" />
-                            <p className="mt-3">Fitness Gym</p>
+                            <p className="mt-3 font-main font-normal text-[18px]">Fitness Gym</p>
                         </div>
                     </div>
                 </div>
