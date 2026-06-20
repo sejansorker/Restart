f593e733b185d36df447801fa716e2389de71f89
sejansorker <sejansorker@gmail.com>
2026-06-21 01:59:01 +0600

    subscribe fixed

--- src/components/Subscribe.jsx
+++ src/components/Subscribe.jsx
@@ -3,25 +3,25 @@ import Container from "./Container";
 
 const Newsletter = () => {
     return (
-        <section className="py-20 bg-[#033238]">
+        <section className="py-15 bg-[#033238]">
             <Container>
-                <div className="bg-[url('/newsletter-bg.jpg')] bg-cover bg-center rounded-md p-10">
+                <div className=" rounded-md p-10">
                     <div className="grid lg:grid-cols-2 gap-8 items-center">
                         <div>
-                            <p className="uppercase text-[#C4995D] text-xs">
+                            <p className="uppercase text-[#C4995D] text-[18px] font-main font-normal">
                                 Subscribe Newsletter
                             </p>
-                            <h2 className="text-white text-5xl font-serif mt-3">
+                            <h2 className="text-white text-[48px] font-main font-bold mt-3">
                                 Get Exclusive Offers & Travel Inspiration
                             </h2>
                         </div>
-                        <div className="flex">
+                        <div className="flex border-1 border-white rounded-lg">
                             <input
                                 type="email"
                                 placeholder="Enter your email address"
-                                className="w-full px-5 py-4 rounded-l-md outline-none text-white"
+                                className="w-full px-5 py-4 rounded-l-md outline-none text-white text-[17px] font-main font-normal"
                             />
-                            <button className="bg-[#0E3440] text-white px-8 rounded-r-md">
+                            <button className="border-l-1 border-white text-[17px] font-main font-bold text-white px-8 rounded-r-md">
                                 Subscribe
                             </button>
                         </div>
